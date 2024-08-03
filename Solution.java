5aa23beba9f9116b1e7053f85ba808190bca1da2
RawatSuraj45 <156521323+RawatSuraj45@users.noreply.github.com>
2024-08-03 13:51:59 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+public class Solution {
+	static int LongestFibonacci(int Arr[], int b) {
+
+		TreeSet<Integer> S = new TreeSet<>();
+		for (int t : Arr) {
+			 
+			S.add(t);
+		}
+		int maxLen = 0, x, y;
+
+		for (int i = 0; i < b; ++i) {
+			for (int j = i + 1; j < b; ++j) {
+
+				x = Arr[j];
+				y = Arr[i] + Arr[j];
+				int len = 3;
+
+				while (S.contains(y) && (y != S.last())) {
+
+					int z = x + y;
+					x = y;
+					y = z;
+					maxLen = Math.max(maxLen, ++len);
+				}
+			}
+		}
+		return maxLen >= 3 ? maxLen : 0;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+		int b = arr.length;
+		System.out.print(LongestFibSubseq(arr, b));
+	}
+}
